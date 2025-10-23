45a983791f83ca5f1139bd47f650aa137cd75585
mrayyan5296 <mrayyan5296@gmail.com>
2025-10-24 03:34:59 +0500

    navbar made

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const Controls: React.FC = () => {
+const Navbar: React.FC = () => {
   return (
-    <div className="controls">
-      <button>New Game</button>
-      <button>Undo</button>
-      <button>Redo</button>
-    </div>
+    <nav className="flex justify-between items-center 
+     bg-white/10 backdrop-blur-md border border-white/50
+     shadow-lg rounded-2xl px-6 py-5 text-white">
+      {/* Left Side */}
+      <div className="flex items-center space-x-2">
+        <span className="font-black text-2xl tracking-wide mr-8">Solitaire</span>
+        <button className="bg-green-500 hover:bg-green-600 px-3 py-1 font-medium rounded-xl">Turn 1</button>
+        <button className="bg-green-500 hover:bg-green-600 px-3 py-1 font-medium rounded-xl">Turn 3</button>
+        <button className="bg-green-500 hover:bg-green-600 px-3 py-1 font-medium rounded-xl">New game</button>
+     
+        <button className="bg-fuchsia-700 hover:bg-fuchsia-600 px-3 py-1 rounded-xl font-black">Hint</button>
+        <button className="bg-yellow-400 hover:bg-yellow-500 text-black px-3 py-1 rounded-xl font-semibold">Undo</button>
+      </div>
+    </nav>
   );
 };
 
-export default Controls;
+export default Navbar;
+ 
