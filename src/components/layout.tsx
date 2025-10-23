0e336a2411d3efcef7410effea97d10246b5058e
mrayyan5296 <mrayyan5296@gmail.com>
2025-10-24 03:13:41 +0500

    layout set

---
+++ src/components/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Controls: React.FC = () => {
+  return (
+    <div className="controls">
+      <button>New Game</button>
+      <button>Undo</button>
+      <button>Redo</button>
+    </div>
+  );
+};
+
+export default Controls;
