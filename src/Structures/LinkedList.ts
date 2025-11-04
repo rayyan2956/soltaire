cfa1e702396727595c30c072443e68e8d448271f
mrayyan5296 <mrayyan5296@gmail.com>
2025-11-05 03:16:38 +0500

    bugs

--- src/Structures/LinkedList.ts
+++ src/Structures/LinkedList.ts
@@ -8,27 +8,38 @@ export class Node<T> {
 
 export class LinkedList<T> {
   head: Node<T> | null = null;
-  tail: Node<T> | null = null;
   size: number = 0;
 
-  add(value: T) {
+  addFirst(value: T) {  // Push at head
     const node = new Node(value);
-    if (!this.head) this.head = this.tail = node;
-    else {
-      this.tail!.next = node;
-      this.tail = node;
-    }
+    node.next = this.head;
+    this.head = node;
     this.size++;
   }
 
-  remove(): T | null {
+  removeFirst(): T | null { // Pop from head
     if (!this.head) return null;
     const value = this.head.value;
     this.head = this.head.next;
-    if (!this.head) this.tail = null;
     this.size--;
     return value;
   }
 
- 
+  peekFirst(): T | null {
+    return this.head ? this.head.value : null;
+  }
+
+  isEmpty(): boolean {
+    return this.size === 0;
+  }
+
+  toArray(): T[] {
+    const arr: T[] = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    return arr;
+  }
 }
